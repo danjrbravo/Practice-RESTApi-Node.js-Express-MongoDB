3bcb44e3d43b69a9792adb71924ed6bdff855480
danjrbravo <danjrbrav0@gmail.com>
2026-04-08 22:04:46 -0500

    Working DB connection with .env file and router contacts file

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost/products')
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to database'))
 
-app.listen(3000, () => console.log('Server started'))
+app.use(express.json())
+
+const contactsRouter = require('./routes/contacts')
+app.use('/contacts',contactsRouter)
+
+app.listen(3000, () => console.log('Server Started'))
